983f18b722d65993cf0c76ef36215ca4e8d9b421
João Vitor Pimentel <joao.pimentel@rbmweb.com.br>
2022-07-07 13:46:30 -0300

    array de teste

--- Colecoes/Program.cs
+++ Colecoes/Program.cs
@@ -2,14 +2,14 @@
 {
   private static void Main(string[] args)
   {
-    int[] arrayInteiros = new int[3];
+    /* int[] arrayInteiros = new int[3];
 
     arrayInteiros[0] = 10;
     arrayInteiros[1] = 20;
-    arrayInteiros[2] = 30;
+    arrayInteiros[2] = 30; */
 
     //arrayInteiros.Length para pegar o total de posições do array
-    System.Console.WriteLine("percorrendo o array pelo for");
+    /* System.Console.WriteLine("percorrendo o array pelo for");
     for (int i = 0; i < arrayInteiros.Length; i++)
     {
       System.Console.WriteLine(arrayInteiros[i]);
@@ -19,6 +19,26 @@
     foreach (int item in arrayInteiros)
     {
       System.Console.WriteLine(item);
+    } */
+
+    //array multidimensional
+
+    int[,] matriz = new int[4, 2]
+    {
+        {8,8},
+        {9,11},
+        {10,10},
+        {11,9}
+    };
+
+    for (int i = 0; i < matriz.GetLength(0); i++)
+    {
+      for (int j = 0; j < matriz.GetLength(1); j++)
+      {
+        System.Console.WriteLine(matriz[i,j]);
+      }
     }
+
+    
   }
 }
